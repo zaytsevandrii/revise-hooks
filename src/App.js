af22ca7cc51c14d3aeb16fe90671fed3dc099876
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-11 17:59:27 +0000

    ref

--- src/App.js
+++ src/App.js
@@ -3,28 +3,13 @@ import './App.css';
 import { useMemo, useState } from 'react';
 
 function App() {
-  const [number,setNumber] = useState(0)
-  const [dark,setDark] = useState(false)
-  console.log(dark)
-  const doubleNumber=useMemo(()=>slowFunction(number),[number])
-  const themeStyles={
-    backgroundColor:dark?'black':'white',
-    color:dark?'white':'black'
-  }
+  
   return (
     <div className="App">
-    <input type="number" value={number} onChange={e=>setNumber(parseInt(e.target.value))} />
-    <button onClick={()=>setDark(prevdark=>!prevdark)}>Change Theme</button>
-    <div style={themeStyles}>{doubleNumber}</div>
+    dsds
     </div>
   );
 }
 
 export default App;
 
-function slowFunction(num){
-  console.log('Calling slow function')
-  for (let i=0; i<=1000000000;i++){}
-    return num*2
-  
-}
