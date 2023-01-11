987db18653be39e5c745addafc6779778978aa67
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-11 17:13:29 +0000

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useMemo, useState } from 'react';
 
 function App() {
+  const [number,setNumber] = useState(0)
+  const [dark,setDark] = useState(false)
+  console.log(dark)
+  const doubleNumber=useMemo(()=>slowFunction(number),[number])
+  const themeStyles={
+    backgroundColor:dark?'black':'white',
+    color:dark?'white':'black'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <input type="number" value={number} onChange={e=>setNumber(parseInt(e.target.value))} />
+    <button onClick={()=>setDark(prevdark=>!prevdark)}>Change Theme</button>
+    <div style={themeStyles}>{doubleNumber}</div>
     </div>
   );
 }
 
 export default App;
+
+function slowFunction(num){
+  console.log('Calling slow function')
+  for (let i=0; i<=1000000000;i++){}
+    return num*2
+  
+}
