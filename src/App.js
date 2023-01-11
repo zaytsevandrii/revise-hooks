30c6366aa30330a90174eb2bbf535a8387c6d5dd
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-11 18:36:21 +0000

    ref full

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 function App() {
-  
+  const [name,setName] =useState('')
+  const inputRef=useRef()
+ const focusFn=()=>{
+  inputRef.current.focus()
+  inputRef.current.value='Some value'
+ }
   return (
-    <div className="App">
-    dsds
+    <div  className="App">
+    <input ref={inputRef} value={name} onChange={e=>setName(e.target.value)}/>
+    <div>My name is {name}</div>
+   <button onClick={focusFn}>Focus</button>
     </div>
   );
 }
